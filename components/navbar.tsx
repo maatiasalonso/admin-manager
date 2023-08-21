c6ae208c629704108d3a52ded79591292332e507
MatiasSM <matias.a.sanmartin@gmail.com>
2023-08-21 19:25:00 -0400

    feature: add search bar on app and mobile modal

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -7,7 +8,21 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Avatar,
+  useDisclosure,
+  Divider,
+} from "@nextui-org/react";
+
 import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
@@ -21,8 +36,76 @@ import clsx from "clsx";
 import { ThemeSwitch } from "@/components/theme-switch";
 
 import { Logo } from "@/components/icons";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [value, setValue] = useState("");
+
+  const searchInput = (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        backdrop="blur"
+        hideCloseButton
+        size="xl"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                <div className="flex items-center gap-3">
+                  <Input
+                    isClearable
+                    variant="bordered"
+                    radius="lg"
+                    placeholder="Type to search..."
+                    startContent={
+                      <MagnifyingGlassIcon className="text-black/50 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0 w-4 h-4" />
+                    }
+                    value={value}
+                    onValueChange={setValue}
+                  />
+                  <Kbd className="h-6 font-semibold text-xs">ESC</Kbd>
+                </div>
+              </ModalHeader>
+              <Divider className="mb-16" />
+              {value.length < 3 && (
+                <ModalBody>
+                  <p className="text-center pb-16">No search results found</p>
+                </ModalBody>
+              )}
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Button
+        aria-label="Search"
+        className="text-sm w-80 justify-between hover:dark:bg-zinc-700 hover:bg-zinc-300"
+        variant="flat"
+        onPress={onOpen}
+        endContent={
+          <div className="flex gap-1 items-center">
+            <Kbd className="hidden lg:inline-block" keys={["command"]}>
+              K
+            </Kbd>
+            or
+            <Kbd className="hidden lg:inline-block" keys={["shift"]}>
+              K
+            </Kbd>
+          </div>
+        }
+      >
+        <div className="flex gap-2">
+          <MagnifyingGlassIcon className="text-base text-default-400 pointer-events-none flex-shrink-0 w-5 h-5" />
+          Search...
+        </div>
+      </Button>
+    </>
+  );
+
   return (
     <NextUINavbar maxWidth="xl" position="sticky" className="shadow-sm">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -32,6 +115,7 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">ACME</p>
           </NextLink>
         </NavbarBrand>
+
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
@@ -56,6 +140,36 @@ export const Navbar = () => {
       >
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
+        </NavbarItem>
+        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        <NavbarItem>
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform w-9 h-9"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">zoey@example.com</p>
+              </DropdownItem>
+              <DropdownItem key="settings">My Settings</DropdownItem>
+              <DropdownItem key="team_settings">Team Settings</DropdownItem>
+              <DropdownItem key="analytics">Analytics</DropdownItem>
+              <DropdownItem key="system">System</DropdownItem>
+              <DropdownItem key="configurations">Configurations</DropdownItem>
+              <DropdownItem key="help_and_feedback">
+                Help & Feedback
+              </DropdownItem>
+              <DropdownItem key="logout" color="danger">
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
       </NavbarContent>
 
